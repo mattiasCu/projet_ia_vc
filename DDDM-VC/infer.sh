6f358a6008bdfdc527413beeafe888dc978dd22f
mattiasCu <101506717+mattiasCu@users.noreply.github.com>
2025-03-12 23:52:34 +0100

    Delete DDDM-VC directory

--- DDDM-VC/infer.sh
+++
@@ -1,8 +0,0 @@
-python3 inference.py \
-    --src_path './DDDM-VC/sample/src_p227_013.wav' \
-    --trg_path './DDDM-VC/sample/tar_p229_005.wav' \
-    --ckpt_model './DDDM-VC/ckpt/model_base.pth' \
-    --ckpt_voc './DDDM-VC/vocoder/voc_ckpt.pth' \
-    --ckpt_f0_vqvae './DDDM-VC/f0_vqvae/f0_vqvae.pth' \
-    --output_dir './DDDM-VC/converted' \
-    -t 6
--- DDDM-VC/inference_2h.sh
+++
@@ -1,7 +0,0 @@
-python3 inference_2h.py \
-    --folder './datasets/src_audio' \
-    --ckpt_model './ckpt/G_520000.pth' \
-    --ckpt_voc './vocoder/voc_ckpt.pth' \
-    --ckpt_f0_vqvae './f0_vqvae/G_720000.pth' \
-    --output_dir './converted' \
-    -t 6
