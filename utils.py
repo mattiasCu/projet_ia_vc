1b3c69794debf0b6d8c8c14b6a3cb904060bb08c
cu <cusui0210@gmail.com>
2025-03-13 00:43:49 +0100

    test

--- utils.py
+++ utils.py
@@ -16,8 +16,8 @@ logger = logging
 
 
 def load_checkpoint(checkpoint_path, model, optimizer=None):
+    print(f"Loading checkpoint from: {checkpoint_path}")
     assert os.path.isfile(checkpoint_path)
-    print(checkpoint_path)
     checkpoint_dict = torch.load(checkpoint_path, map_location='cpu')
     iteration = checkpoint_dict['iteration']
     learning_rate = checkpoint_dict['learning_rate']
