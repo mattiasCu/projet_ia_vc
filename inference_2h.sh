892a36f72b0b9f8201e85bd2f48f0cc775400079
cu <cusui0210@gmail.com>
2025-03-13 01:35:17 +0100

    test

--- inference_2h.sh
+++ inference_2h.sh
@@ -1,7 +1,7 @@
 python3 inference_2h.py \
     --folder '/home/ensta/ensta-sui/workplace/data/' \
-    --ckpt_model './ckpt/G_520000.pth' \
+    --ckpt_model './ckpt/model_base.pth' \
     --ckpt_voc './vocoder/voc_ckpt.pth' \
-    --ckpt_f0_vqvae './f0_vqvae/G_720000.pth' \
+    --ckpt_f0_vqvae './f0_vqvae/f0_vqvae.pth' \
     --output_dir './converted' \
     -t 6
